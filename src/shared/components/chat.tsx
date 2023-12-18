a7e40c2327c039c61ffa238f294193c321b85eb0
shavvimal <shavhugan@gmail.com>
2023-12-18 18:12:04 +0000

    routing

--- src/shared/components/chat.tsx
+++ src/shared/components/chat.tsx
@@ -54,7 +54,7 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       },
       onFinish() {
         if (!path.includes("chat")) {
-          router.push(`/chat/${id}`, { shallow: true, scroll: false });
+          router.push(`/chat/${id}`);
           router.refresh();
         }
       },
